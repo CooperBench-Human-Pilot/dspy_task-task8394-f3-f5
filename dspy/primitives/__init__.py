3ddbc0d3e6ac4c51d7451892c8dbe2d062a581fd
Arya Prabhudesai <apprabhudesai98@gmail.com>
2026-05-29 16:56:09 -0700

    Base at 3ded7a6266514c1569452b0e26bcd4e75c1ee6db

---
+++ dspy/primitives/__init__.py
@@ -0,0 +1,15 @@
+from dspy.primitives.example import Example
+from dspy.primitives.module import BaseModule
+from dspy.primitives.prediction import Completions, Prediction
+from dspy.primitives.program import Module, Program
+from dspy.primitives.python_interpreter import PythonInterpreter
+
+__all__ = [
+    "Example",
+    "BaseModule",
+    "Prediction",
+    "Completions",
+    "Program",
+    "Module",
+    "PythonInterpreter",
+]
